3743d612e1312bc219ef98d22bba222e52cdfbba
ibrahim <ibrahimayad321@gmail.com>
2021-01-15 16:12:13 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id = "#app">
-      <div :v-for = "item in test">
-        <div>
-          <h1>{{item.name}}</h1>
-          <h1>{{item.age}}</h1>
-          <h1>{{item.work}}</h1>
-        </div>
+  <div id="#app">
+    <div v-for="item in test" v-bind:key="item">
+      <div>
+        <h1>{{ item.name }}</h1>
+        <h1>{{ item.age }}</h1>
+        <h1>{{ item.work }}</h1>
       </div>
+    </div>
   </div>
 </template>
 <script>
-  export default {
-    data(){
-      return{
-        test:{
-          name:"ali",
-          age:200,
-          work: "true"
-        }
-      }
-    }  
-  }
+export default {
+  data() {
+    return {
+      test: [
+        {
+          name: "ali",
+          age: 200,
+          work: "true",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
